a2cb187c588f22b3a230f55d05c0569a30b30252
Don Woodford <dwoodford@ITAGROUP.com>
2021-01-15 20:04:54 -0600

    Initial

---
+++ BlazorApp50/Data/WeatherContext.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorApp50.Data
+{
+    public class WeatherContext : DbContext
+    {
+        public WeatherContext()
+        {
+
+        }
+        public WeatherContext(DbContextOptions<WeatherContext> options) : base(options)
+        {
+
+        }
+
+        DbSet<WeatherReport> WeatherReports { get; set; }
+    }
+
+    public class WeatherReport
+    {
+        public WeatherReport()
+        {
+            WeatherReportId = Guid.NewGuid();
+        }
+
+        public Guid WeatherReportId { get; private set; }
+        public decimal TemperatureF { get; set; }
+        public decimal DewPointF { get; set; }
+        public string Summary { get; set; }
+        public int WindSpeedMPH { get; set; }
+        public string WindDirection { get; set; }
+    }
+}
---
+++ BlazorApp50/Migrations/20210116011217_InitialCreate.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BlazorApp50.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "WeatherReports",
+                columns: table => new
+                {
+                    WeatherReportId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    TemperatureF = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    DewPointF = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    Summary = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    WindSpeedMPH = table.Column<int>(type: "int", nullable: false),
+                    WindDirection = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_WeatherReports", x => x.WeatherReportId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "WeatherReports");
+        }
+    }
+}
