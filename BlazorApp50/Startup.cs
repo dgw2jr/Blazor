03cc30b62e0803024a83dd95930f12f96e37fbbb
CSchneider <ionprojekt@live.com>
2021-01-21 20:29:45 -0600

    Add razor compilation for dev env so front end changes don't need to be recompiled

--- BlazorApp50/Startup.cs
+++ BlazorApp50/Startup.cs
@@ -14,18 +14,25 @@ namespace BlazorApp50
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        public Startup(IConfiguration configuration, IWebHostEnvironment env)
         {
             Configuration = configuration;
+            Env = env;
         }
 
         public IConfiguration Configuration { get; }
+        public IWebHostEnvironment Env { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddRazorPages();
+            var razorPagesBuilder = services.AddRazorPages();
+            if (Env.IsDevelopment())
+            {
+                razorPagesBuilder.AddRazorRuntimeCompilation();
+            }
+
             services.AddServerSideBlazor();
 
             services.AddDbContext<WeatherContext>(options => options.UseSqlServer(Configuration.GetConnectionString("WeatherContext")));
