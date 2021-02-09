678ddbc4a095eaaf5d570de3e6b2ad2e0c01e5d7
CSchneider <ionprojekt@live.com>
2021-02-08 23:32:12 -0600

    Fix errors

--- BlazorApp50.Microservices.Traffic/Program.cs
+++ BlazorApp50.Microservices.Traffic/Program.cs
@@ -1,11 +1,8 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.IO;
+using System.Reflection;
 
 namespace BlazorApp50.Microservices.Traffic
 {
@@ -18,7 +15,7 @@ namespace BlazorApp50.Microservices.Traffic
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
-ConfigureAppConfiguration((hostingContext, config) =>
+            .ConfigureAppConfiguration((hostingContext, config) =>
             {
                 config
                     .SetBasePath(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location))
