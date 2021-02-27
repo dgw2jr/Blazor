6d56f509c6da136ad8950e129b1df29d7bef578e
CSchneider <ionprojekt@live.com>
2021-02-26 20:57:14 -0600

    Add ant blazor

--- BlazorApp50/Startup.cs
+++ BlazorApp50/Startup.cs
@@ -53,6 +53,8 @@ namespace BlazorApp50
             });
             services.AddMediator();
             services.AddMassTransitHostedService();
+
+            services.AddAntDesign();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
